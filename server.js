4e5bb0e79eeee3b85208029090248ee7cad25b8f
jhoun <jayhoun@gmail.com>
2016-12-29 11:35:04 -1000

    implemented helium request

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const net = require('net');
 const index = require('./HTML-files/index');
+const hydrogen = require('./HTML-files/hydrogen');
 
-console.log(index);
+
 let server = net.createServer((client) => {
   //recieves client from data
   client.on('data', (data) =>{
     var wordArr = data.toString().split(' ');
+    console.log(wordArr);
+
+    //finds '/' and 'index.html'
     for (var i = 0; i < wordArr.length; i++){
       if (wordArr[i] === "GET" && wordArr[i + 1] === '/' || wordArr[i + 1] === "/index.html"){
         // process.stdout.write(data);
@@ -15,9 +19,25 @@ let server = net.createServer((client) => {
           Content-Length: ${index.length}
           Connection: keep-alive\n\n`);
         client.write(index);
-        client.end();
+
       }
     }
+
+    //finds 'hydrogen'
+    for (var i = 0; i < wordArr.length; i++){
+      if (wordArr[i] === "GET" && wordArr[i + 1] === "/index/hydrogen.html"){
+        // process.stdout.write(data);
+        client.write(`HTTP/1.1 200 OK
+          Date: ${new Date()};
+          Content-Type: text/html; charset=utf-8
+          Content-Length: ${hydrogen.length}
+          Connection: keep-alive\n\n`);
+        client.write(hydrogen);
+      }
+    }
+
+    //ends the request
+    client.end();
   });
 
   client.on('end', () => {
