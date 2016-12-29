34a9664bf8a3f43f3a9e181a2b9bbd102bcebf74
jhoun <jayhoun@gmail.com>
2016-12-29 11:44:43 -1000

    implemented hydrogen request

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const net = require('net');
 const index = require('./HTML-files/index');
 const hydrogen = require('./HTML-files/hydrogen');
+const helium = require('./HTML-files/helium');
+const error404 = require('./HTML-files/404');
 
 
 let server = net.createServer((client) => {
@@ -33,6 +35,32 @@ let server = net.createServer((client) => {
           Content-Length: ${hydrogen.length}
           Connection: keep-alive\n\n`);
         client.write(hydrogen);
+      }
+    }
+
+    //finds 'helium'
+    for (var i = 0; i < wordArr.length; i++){
+      if (wordArr[i] === "GET" && wordArr[i + 1] === "/index/helium.html"){
+        // process.stdout.write(data);
+        client.write(`HTTP/1.1 200 OK
+          Date: ${new Date()};
+          Content-Type: text/html; charset=utf-8
+          Content-Length: ${helium.length}
+          Connection: keep-alive\n\n`);
+        client.write(helium);
+      }
+    }
+
+    //'error 404' if nothing is found
+    for (var i = 0; i < wordArr.length; i++){
+      if (wordArr[i] === "GET" && wordArr[i + 1] === undefined){
+        // process.stdout.write(data);
+        client.write(`HTTP/1.1 200 OK
+          Date: ${new Date()};
+          Content-Type: text/html; charset=utf-8
+          Content-Length: ${error404.length}
+          Connection: keep-alive\n\n`);
+        client.write(error404);
       }
     }
 
