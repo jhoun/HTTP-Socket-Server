cde7307cdf5280b1dc44aa7ab972aded79547ed1
jhoun <jayhoun@gmail.com>
2016-12-29 19:07:35 -1000

    replaced GET path with variable

--- client.js
+++ client.js
@@ -1,22 +1,23 @@
 const net = require('net');
 
 const EVENT_DATA = 'data';
-const host = process.argv[2];
+const host = process.argv[2].split('/')[0];
 const hostPath = process.argv[2].split('/')[1];
+console.log(host);
 let port;
 
-console.log(hostPath);
-if (host !== 'localhost'){
-  port = 80;
-} else {
+// console.log(hostPath);
+if (host === 'localhost'){
   port = 8080;
+} else {
+  port = 80;
 }
 
 const client = net.connect(port, host, () => {
 
 
   process.stdin.on( EVENT_DATA, () => {
-    client.write(`GET / HTTP/1.1
+    client.write(`GET /hostPath HTTP/1.1
 Date: ${new Date}
 Host: localhost
 User-Agent: jay\r\n\r\n`);
